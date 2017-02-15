3d84c456d9ff09e25e5a89c5da964ad7207e4808
Devin <Devin@192.168.123.4>
2017-02-15 13:27:20 -0600

    Add another print in Main class

--- src/consoleUI/Console.java
+++ src/consoleUI/Console.java
@@ -4,5 +4,6 @@ package consoleUI;
 public class Console {
     public static void main(String[] args) {
         System.out.println("Hello Github!");
+        System.out.println("Yay creating repos!");
     }
 }
